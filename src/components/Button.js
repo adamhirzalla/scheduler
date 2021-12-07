386d15c0aca661004d83cb25d7492749db8086e3
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-12-07 00:51:10 -0500

    add Button component with storybook testing for Base/Confirm/Danger/Clickable/Disabled

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,18 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   const { children, confirm, danger, disabled, onClick } = props
+   let buttonClass = "button"
+   buttonClass += 
+      confirm ? " button--confirm" :
+      danger ? " button--danger" :
+      ""
+
+   return (
+      <button 
+      className={buttonClass}
+      onClick={onClick && onClick}
+      disabled={disabled && disabled}
+      >{children}</button>
+   );
 }
