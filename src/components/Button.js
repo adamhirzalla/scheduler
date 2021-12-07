d72321a89531e0c38f43e16d06743e456a8c9798
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-12-07 01:12:54 -0500

    import classnames library and using it for custom conditional Button classes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,14 @@
 import React from "react";
+import classNames from "classnames"
 
 import "components/Button.scss";
 
 export default function Button(props) {
    const { children, confirm, danger, disabled, onClick } = props
-   let buttonClass = "button"
-   buttonClass += 
-      confirm ? " button--confirm" :
-      danger ? " button--danger" :
-      ""
+   let buttonClass = classNames('button', {
+         "button--confirm": confirm, 
+         "button--danger": danger
+      })
 
    return (
       <button 
