a6193bd70899231723fc6621a46bea431187aa1d
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-12-07 06:28:03 -0500

    add sass styles to DayListItem component + use classNames to dynamically set classes

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,11 +1,19 @@
 import React from "react";
+import classNames from "classnames";
+
+import "components/DayListItem.scss"
 
 export default function DayListItem(props) {
   const { name, spots, selected, setDay } = props
   const handleClick = () => setDay(name)
 
+  let dayClass = classNames('day-list__item', {
+    'day-list__item--selected': selected,
+    'day-list__item--full': spots === 0,
+  })
+
   return (
-    <li onClick={handleClick}>
+    <li className={dayClass} onClick={handleClick}>
       <h2 className="text--regular">{name}</h2> 
       <h3 className="text--light">{spots} spots remaining</h3>
     </li>
