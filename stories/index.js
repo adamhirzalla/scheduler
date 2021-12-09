3fbc17bdd2d86e92f20996d7e547d52228962109
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-12-08 23:10:42 -0500

    add Show & Confirm components + stories for each

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,8 @@ import InterviewerList from "components/InterviewerList"
 import Appointment from "components/Appointment/index"
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 storiesOf("Button", module)
   .addParameters({
@@ -136,3 +138,18 @@ storiesOf("InterviewerList", module)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add ("Header", () => <Header time="12pm" />)
   .add ("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add ("Show", () => (
+    <Show {...{
+      student: "Lydia Miller-Jones",
+      interviewer,
+      onEdit: action("onEdit"),
+      onDelete: action("onDelete")
+    }} />
+  ))
+  .add ("Confirm", () => (
+    <Confirm {...{
+      message: "Delete the appointment?",
+      onConfirm: action("onConfirm"),
+      onCancel: action("onCancel")
+    }} />
+  ))
