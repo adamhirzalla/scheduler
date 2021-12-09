713d90b5c1fffddcb85be1331615c2156815d62e
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-12-09 00:44:27 -0500

    display Appointment component to render with appropriate children

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,19 @@
 import React from "react";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 import "components/Appointment/styles.scss"
 
 export default function Appointment(props) {
-  const { time } = props
+  const { time, interview } = props
+
   return (
     <article className="appointment">
-      {time ? `Appointment at ${time}` : `No Appointments`}
+      <Header {...{time}} />
+      {interview ? 
+        <Show {...interview}/> : 
+        <Empty />}
     </article>
   )
 }
