3a37029951e765241c5412cd0dc031c94022a1e8
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-12-11 00:38:30 -0500

    add new selector getInterviewersForDay to return an array of interviewer objects

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -9,6 +9,17 @@ export function getAppointmentsForDay(state, name) {
   return appointments
 }
 
+export function getInterviewersForDay(state, name) {
+  const [filtered] = state.days.filter(day => day.name === name)
+  const interviewers = []
+  if (filtered) {
+    for (const interviewerId of filtered.interviewers) {
+      interviewers.push(state.interviewers[interviewerId])
+    }
+  }
+  return interviewers
+}
+
 export function getInterview(state, interview) {
   if (!interview) return null
   const { student, interviewer } = interview
