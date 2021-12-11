113b4d1ad2d1d665bd58c4cf4ff4285bae0424a9
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-12-11 00:12:58 -0500

    handle transitioning to Form component and back to Empty using VisualMode custom hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,28 +2,43 @@ import React from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 
 import "components/Appointment/styles.scss"
 import useVisualMode from "hooks/useVisualMode";
 
 const EMPTY = "EMPTY"
 const SHOW = "SHOW"
+const CREATE = "CREATE"
+const SAVE = "SAVE"
+const EDIT = "EDIT"
+const DELETE = "DELETE"
 
 export default function Appointment(props) {
   const { time, interview } = props
   const { transition, back, mode } = useVisualMode(interview ? SHOW : EMPTY)
   const handleClick = (e) => console.log(`Clicked ${e.target.alt}`);
+  const onAdd = () => transition(CREATE)
+  const onCancel = () => back()
+  const onSave = () => transition(SAVE)
+  const onEdit = () => transition(EDIT)
+  const onDelete = () => transition(DELETE)
 
   return (
     <article className="appointment">
       <Header {...{time}} />
       {
-      mode === SHOW ? <Show {...{
-        ...interview, 
-        onEdit: handleClick,
-        onDelete: handleClick
-      }}/> : 
-      mode === EMPTY ? <Empty onAdd={handleClick}/> : <></>
+        mode === SHOW ? <Show {...{
+          ...interview, 
+          onEdit: handleClick,
+          onDelete: handleClick
+        }}/> : 
+        mode === CREATE ? <Form {...{
+          interviewers: [],
+          onSave: handleClick,
+          onCancel
+        }}/> :
+        mode === EMPTY ? <Empty {...{onAdd}}/> : <></>
       }
     </article>
   )
