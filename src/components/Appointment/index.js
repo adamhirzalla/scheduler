9e2b03f01068ee994433cf602bf4a1fc1f029bef
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-12-10 23:54:29 -0500

    render Show and Empty components based on mode from custom VisualMode hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,16 +4,27 @@ import Show from "./Show";
 import Empty from "./Empty";
 
 import "components/Appointment/styles.scss"
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY"
+const SHOW = "SHOW"
 
 export default function Appointment(props) {
   const { time, interview } = props
+  const { transition, back, mode } = useVisualMode(interview ? SHOW : EMPTY)
+  const handleClick = (e) => console.log(`Clicked ${e.target.alt}`);
 
   return (
     <article className="appointment">
       <Header {...{time}} />
-      {interview ? 
-        <Show {...interview}/> : 
-        <Empty />}
+      {
+      mode === SHOW ? <Show {...{
+        ...interview, 
+        onEdit: handleClick,
+        onDelete: handleClick
+      }}/> : 
+      mode === EMPTY ? <Empty onAdd={handleClick}/> : <></>
+      }
     </article>
   )
 }
