90cd027f3bead0a2f09c9a9c520408abdc1e9908
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-12-13 15:14:58 -0500

    clean up extra spaces

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,23 +1,19 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  // const [mode, setMode] = useState(initial)
   const [history, setHistory] = useState([initial])
-
   const transition = (newMode, replace = false) => {
     setHistory(prev => {
       if (replace) return [newMode, ...prev.slice(1)]
       return [newMode, ...prev]
     })
   }
-
   const back = () => {
     setHistory(prev => {
       if (prev.length <= 1) return prev
       return prev.slice(1)
     })
   }
-
   const mode = history[0]
   return { mode, transition, back }
 }
