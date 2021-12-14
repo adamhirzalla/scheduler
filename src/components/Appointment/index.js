b12d79cad2478f844ad5d6110008b9fac1d5a7c0
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-12-13 21:46:21 -0500

    add confirmation dialog for deleting an interview

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,18 +7,21 @@ import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
 import Status from "./Status";
+import Confirm from "./Confirm";
 
 const EMPTY = "EMPTY"
 const SHOW = "SHOW"
 const CREATE = "CREATE"
 const SAVING = "SAVING"
 const DELETING = "DELETING"
+const CONFIRM = "CONFIRM"
 
 export default function Appointment(props) {
   const { id, time, interview, interviewers, bookInterview, deleteInterview } = props
   const { transition, back, mode } = useVisualMode(interview ? SHOW : EMPTY)
   const handleClick = (e) => console.log(`Clicked ${e.target.alt}`);
   const onAdd = () => transition(CREATE)
+  const onDelete = () => transition(CONFIRM)
   const onCancel = () => back()
 
   const onSave = (student, interviewer) => {
@@ -31,7 +34,7 @@ export default function Appointment(props) {
     .then(() => transition(SHOW))
   }
 
-  const onDelete = () => {
+  const onConfirm = () => {
     transition(DELETING)
     deleteInterview(id)
     .then(() => transition(EMPTY))
@@ -53,6 +56,11 @@ export default function Appointment(props) {
         }}/> :
         mode === SAVING ? <Status message={'Saving, please wait...'} /> :
         mode === DELETING ? <Status message={'Deleting, please wait...'} /> :
+        mode === CONFIRM ? <Confirm {...{
+          message: 'Are you sure you want to delete?',
+          onConfirm,
+          onCancel
+        }} /> :
         mode === EMPTY ? <Empty {...{onAdd}}/> : <></>
       }
     </article>
