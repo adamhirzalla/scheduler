f0c833a6c5aac333076c99b6aac561efb5cc0904
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-12-15 02:51:18 -0500

    fix issue with jesting while using websocket connection to use env var

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -7,5 +7,7 @@ import Application from "components/Application";
 afterEach(cleanup);
 
 it("renders without crashing", () => {
+  const ENV_VAR = process.env
+  process.env.REACT_APP_WEBSOCKET_URL = 'ws://localhost:8001'
   render(<Application />);
 });
